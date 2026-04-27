73a807f23e4b25226f9ae402a6a523adf7c00163
letfonsecaa <leticia.santos@sptech.school>
2026-04-27 00:10:03 -0300

    ajuste p/ backend

--- app/db/models.py
+++ app/db/models.py
@@ -12,7 +12,7 @@ class Transacao(Base):
     __tablename__ = "transacao"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    usuario_id = Column(Integer, nullable=False)
+    usuario_id = Column(Integer, nullable=True)
     honorario_id = Column(Integer, nullable=True)
     titulo = Column(String(150))
     valor = Column(Numeric(10, 2))
@@ -21,7 +21,7 @@ class Transacao(Base):
     status_aprovacao = Column(String(50))
     data_emissao = Column(Date)
     data_vencimento = Column(Date)
-    data_pagamento = Column(String(10), nullable=False)
+    data_pagamento = Column(Date, nullable=False)
     descricao = Column(Text)
     observacoes = Column(Text)
     contraparte = Column(String(150))
@@ -33,7 +33,7 @@ class ImportacaoHistorico(Base):
     __tablename__ = "importacao_historico"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    usuario_id = Column(Integer, nullable=False)
+    usuario_id = Column(Integer, nullable=True)
     tipo = Column(String(50), nullable=False)
     arquivo = Column(String(255), nullable=False)
     data = Column(DateTime, server_default=func.current_timestamp())
